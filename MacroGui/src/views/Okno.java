5a5d9da808a6be1301cd51eb1c4cfd7811cd16b3
nmietus <nmietus93@gmail.com>
2018-05-28 20:09:10 +0200

    Odczyt współrzędnych kursora.

--- MacroGui/src/views/Okno.java
+++ MacroGui/src/views/Okno.java
@@ -1,8 +1,13 @@
 package views;
 
+import java.awt.AWTException;
 import java.awt.BorderLayout;
+import java.awt.Robot;			//klasa Robot
+import java.awt.event.*;		//po³o¿enie myszki i inne
 
 import java.awt.EventQueue;
+import java.awt.MouseInfo;
+import java.awt.Point;
 
 import javax.swing.Timer;
 import javax.swing.JFrame;
@@ -12,14 +17,23 @@ import javax.swing.JTextField;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
 
 public class Okno extends JFrame 
 {
 
 	private JPanel contentPane;
-	private JTextField textField;
 	
 	int i;
+	Robot robot;
+	Point punkt;
+	Timer t;
+	private JLabel lblWspdne;
+	private JLabel lblWsprzdne;
 
 	/**
 	 * Launch the application.
@@ -48,28 +62,49 @@ public class Okno extends JFrame
 	public Okno() 
 	{
 		i=0;
-		setIconImage(Toolkit.getDefaultToolkit().getImage(Okno.class.getResource("/resources/robot-arm.png")));
-		setTitle("Robot");
 		initElements();
 		initEvents();
+		t.start();
 	}
 	///////////////////////////////////////////////////////////////////////////
 	//DEKLARACJE I INICJALIZACJE ELEMENTÓW
 	///////////////////////////////////////////////////////////////////////////
 	public void initElements() 
 	{
+		try {
+			robot = new Robot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setAutoscrolls(true);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Okno.class.getResource("/resources/robot-arm.png")));
+		setTitle("Robot");
 		
-		textField = new JTextField();
-		textField.setBounds(10, 22, 135, 54);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		lblWspdne = new JLabel("Wsp\u00F3\u0142rz\u0119dne");
+		lblWspdne.setBounds(341, 237, 71, 14);
+		contentPane.add(lblWspdne);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(24, 11, 147, 102);
+		contentPane.add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		textArea.setLineWrap(true);
+		
+		lblWsprzdne = new JLabel("Wsp\u00F3\u0142rz\u0119dne:");
+		lblWsprzdne.setBounds(341, 212, 83, 14);
+		contentPane.add(lblWsprzdne);
 	}
 	
 	///////////////////////////////////////////////////////////////////////////
@@ -77,11 +112,13 @@ public class Okno extends JFrame
 	///////////////////////////////////////////////////////////////////////////
 	public void initEvents() 
 	{
-		Timer t = new Timer(1, new ActionListener() 
+		t = new Timer(1, new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
 			{
-				
+				//setLocation(punkt);
+				punkt = MouseInfo.getPointerInfo().getLocation();
+				lblWspdne.setText(punkt.getX()+" : "+punkt.getY());
 			}
 		});
 	}
